de72d66e66651889f78c9daee04de1e826698015
masonjar <mason.whitaker79@gmail.com>
2021-12-01 08:18:12 -0800

    Created HornedBeast Component and rendered it in the Main Custom Component

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import HornedBeast from './HornedBeast';
 
 class Main extends React.Component {
     render () {
       return (
         <>
         <p>Horned Beasts coming soon...</p>
+        <HornedBeast />
         </>
       )
     }
