a5c1eb3f0eb4a4a31686c672188b6559f1e6e1df
masonjar <mason.whitaker79@gmail.com>
2021-12-01 08:07:59 -0800

    Created Header.js Custom  Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-// import logo from './logo.svg';
+import Header from './Header';
 import './App.css';
 
 class App extends React.Component {
   render () {
     return (
       <div className="App">
-          <h1>Hello <strong>World!</strong>, I am here to <strong>CONQUER</strong> this puny world!</h1>
+          <h1>Hello World!, I am here to <strong>CONQUER</strong> this puny world!</h1>
           <h6>*dies*</h6>
+          <Header />
       </div>
     )
   }
