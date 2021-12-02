8630f2a5daf23db12057f0b8b20e80906ea8ddeb
masonjar <mason.whitaker79@gmail.com>
2021-12-02 08:51:41 -0800

    Converted Beast component to a  bootstrap card. Added CSS to change the size.

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,18 +1,32 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
 // import beastData from "props";
 
 class HornedBeast extends React.Component {
   render() {
     return (
       <div className="beast">
-        <h2>{this.props.beast.title}</h2>
+        <Card>
+          <Card.Body>
+            <Card.Title>{this.props.beast.title}</Card.Title>
+            <Card.Img
+              variant="top"
+              src={this.props.beast.image_url}
+              alt={this.props.beast.description}
+              title={this.props.beast.title}
+              style={{ width: "20%" }}
+            />
+            <Card.Text>{this.props.beast.description}</Card.Text>
+          </Card.Body>
+        </Card>
+        {/* <h2>{this.props.beast.title}</h2>
         <img
           src={this.props.beast.image_url}
           alt={this.props.beast.description}
           title={this.props.beast.title}
           style={{ width: "20%" }}
         />
-        <p>{this.props.beast.description}</p>
+        <p>{this.props.beast.description}</p> */}
       </div>
     );
   }
