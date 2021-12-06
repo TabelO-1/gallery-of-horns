704acb499d53d58d723ef3a7328efdd1a85e5682
masonjar <mason.whitaker79@gmail.com>
2021-12-06 08:01:11 -0800

    removed redunant css and add a botostrap container component to the hornedbeast component.

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,17 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
 import beastData from "./beastData.json";
+import Container from "react-bootstrap/Container";
 
 class Main extends React.Component {
   render() {
     return (
-      <>
+      <Container>
         <p>Horned Beasts coming soon...</p>
         {beastData.map((beast, idx) => (
           <HornedBeast key={idx} beast={beast} />
         ))}
-      </>
+      </Container>
     );
   }
 }
