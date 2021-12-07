d25c22b5d27e1217afcb894f93776d41af1fb486
masonjar <mason.whitaker79@gmail.com>
2021-12-07 07:48:25 -0800

    Addedd constructor for state and addfavorites() updater. added text for favorites

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 // import beastData from "props";
-
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites: 0,
+    };
+  }
+  addFavorites = () => {
+    this.setState({ favorites: this.state.favorites + 1 });
+  };
   render() {
     return (
       <div className="beast">
-        <Card>
+        <Card onClick={this.addFavorites}>
           <Card.Body>
             <Card.Title>{this.props.beast.title}</Card.Title>
             <Card.Img
@@ -17,16 +25,9 @@ class HornedBeast extends React.Component {
               style={{ width: "20%" }}
             />
             <Card.Text>{this.props.beast.description}</Card.Text>
+            <Card.Text>🔥 = {this.state.favorites}</Card.Text>
           </Card.Body>
         </Card>
-        {/* <h2>{this.props.beast.title}</h2>
-        <img
-          src={this.props.beast.image_url}
-          alt={this.props.beast.description}
-          title={this.props.beast.title}
-          style={{ width: "20%" }}
-        />
-        <p>{this.props.beast.description}</p> */}
       </div>
     );
   }
