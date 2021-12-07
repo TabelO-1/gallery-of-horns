0a9a82dcf4dd67a86cdccd0e09fa54e81533526d
masonjar <mason.whitaker79@gmail.com>
2021-12-07 07:40:07 -0800

    Added row component, the container component was already added on the card branch

--- src/Main.js
+++ src/Main.js
@@ -2,15 +2,18 @@ import React from "react";
 import HornedBeast from "./HornedBeast";
 import beastData from "./beastData.json";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 class Main extends React.Component {
   render() {
     return (
       <Container>
         <p>Horned Beasts coming soon...</p>
-        {beastData.map((beast, idx) => (
-          <HornedBeast key={idx} beast={beast} />
-        ))}
+        <Row xs={1} md={2} lg={3} xl={4}>
+          {beastData.map((beast, idx) => (
+            <HornedBeast key={idx} beast={beast} />
+          ))}
+        </Row>
       </Container>
     );
   }
