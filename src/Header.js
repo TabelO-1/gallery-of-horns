1ba0aa5acd6155d3dcf88ccedc44034f8ef7dd73
masonjar <mason.whitaker79@gmail.com>
2021-12-08 08:05:25 -0800

    Added styling. Though it's i's imcomplete

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ class Header extends React.Component {
   render() {
     return (
       <>
-        <h1>Gallery of Horns</h1>
+        <h1 className="header">Gallery of Horns</h1>
       </>
     );
   }
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -13,8 +13,12 @@ class HornedBeast extends React.Component {
   };
   render() {
     return (
-      <div className="beast">
-        <Card onClick={this.addFavorites}>
+      <div>
+        <Card
+          className="beast"
+          style={{ width: "18rem" }}
+          onClick={this.addFavorites}
+        >
           <Card.Body>
             <Card.Title>{this.props.beast.title}</Card.Title>
             <Card.Img
@@ -22,8 +26,9 @@ class HornedBeast extends React.Component {
               src={this.props.beast.image_url}
               alt={this.props.beast.description}
               title={this.props.beast.title}
+              className="beastImg"
             />
-            <Card.Text>{this.props.beast.description}</Card.Text>
+            <Card.Text className="beastDescription">{this.props.beast.description}</Card.Text>
             <Card.Text>🔥 = {this.state.favorites}</Card.Text>
           </Card.Body>
         </Card>
