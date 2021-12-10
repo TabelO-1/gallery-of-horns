d609b82785e1532450eed860e62e5b8a1f2fdcaf
masonjar <mason.whitaker79@gmail.com>
2021-12-10 07:37:49 -0800

    Added the FilterForm.js into the Main.js.

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
+import FilterForm from "./FilterForm";
 import beastData from "./beastData.json";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -8,6 +9,7 @@ class Main extends React.Component {
   render() {
     return (
       <Container>
+        <FilterForm />
         <Row xs={1} sm={2} md={3} lg={4} xl={5}>
           {beastData.map((beast, idx) => (
             <HornedBeast key={idx} beast={beast} />
